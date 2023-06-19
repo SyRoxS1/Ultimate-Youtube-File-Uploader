af2b1bf9a2fbf8fcdcd4c9b691662541136934da
SyRoxS <lmoreaumenanteau@gmail.com>
2023-06-19 11:16:40 +0200

    Youtube as cloud is up and ready

--- VideoToFileNormal.py
+++ VideoToFileNormal.py
@@ -36,7 +36,7 @@ def VideoToFileNormal(nomfichier,final):
         print("lecture de l'image",count+1,"sur",len(images),end="\r")
         count += 1
         temp= cv2.imread("./"+image_folder+"/"+image)
-        start_time = time.time()
+        
         for i in range(0,720,2):
             for j in range(0,1280,2):
                 pixel= temp[i, j]
@@ -58,10 +58,7 @@ def VideoToFileNormal(nomfichier,final):
                 else:
                     bin.append(1)
                 moyenne = 0
-        stop_time = time.time()
-        print(stop_time-start_time)
-    
-    start_time = time.time()  
+        
     bin = ''.join(map(str,bin))
     b = bin.find('011101110111011101110111001011100111100101101111011101010111010001110101011000100110010100101110011000110110111101101101001011110110110101110010011001100111001001101001011100000110111101101110')
     bin = bin[:b]
@@ -70,9 +67,8 @@ def VideoToFileNormal(nomfichier,final):
     
     binary_string_to_file(bin, final)
     
-    stop_time = time.time()
-    print(stop_time-start_time)
-    print("\nProblems : ",error)
+    
+    print("\nClose pixels (shouldn't be an issue) : ",error)
     
     print("Vidéo convertie en fichier réussie !\n")
     for filename in os.listdir(image_folder):
--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from VideoToFileNormal import VideoToFileNormal
 from VideoToFileBig import VideoToFileBig
 from GetYtVideosName import get_channel_videos
 from UploadeToYt import upload
+from VideoDownloader import DLFromYt
 import os
 
 if __name__ == '__main__':
@@ -50,7 +51,7 @@ if __name__ == '__main__':
         channel_url = input('Enter a YouTube channel URL: ')
         
         urls = get_channel_videos(channel_url)
-        print(urls["2"][1])
+        print(urls)
         ChoixCloud = int(input("Ajouter des fichiers (1), Extraire des fichiers(2), Afficher les fichiers (3) : "))
         if ChoixCloud == 1:
             ChoixCompr = int(input(f"Niveau de sureté voulu ? : \n 1 : Faible (plus de risque de corruption mais plus rapide)\n2 : Fort (Meme vitesse mais plus gros fichier vidéo)\n"))
@@ -61,7 +62,7 @@ if __name__ == '__main__':
                 
                 video_name = fichiervoulue + '.mp4'
                 ImagesToVideo(video_name)
-                upload(video_name,video_name,'Compr1','22','','private')
+                upload(video_name,video_name[:-4],'Compr1','22','','public')
                 
             if ChoixCompr == 2:
                 fichiervoulue = str(input("Nom du fichier a convertire en vidéo (sans oublier l'extension) : \n"))
@@ -69,7 +70,7 @@ if __name__ == '__main__':
     
                 video_name = fichiervoulue + '.mp4'
                 ImagesBigToVideo(video_name)
-                upload(video_name,video_name,'Compr2','22','','private')
+                upload(video_name,video_name[:-4],'Compr2','22','','public')
             for filename in os.listdir('./'):
                 if filename.endswith('.mp4'):
                     file_path = os.path.join('./', filename)
@@ -81,8 +82,25 @@ if __name__ == '__main__':
             ChoixVid = input("Selectionner le numéro associé au fichier a extraire : ")
             urlDL = urls[ChoixVid][0]
             print(urlDL)
+            DLFromYt(urlDL)
+            if urls[ChoixVid][3] == "Compr1":
+                nomVid = urls[ChoixVid][1].replace(".","") + ".mp4"
+                print(nomVid)
+                Vid = os.path.join('./videos',nomVid)
+                VideoToFileNormal(Vid,urls[ChoixVid][1])
+                
+                os.remove(Vid)
+                print(f"Deleted: {file_path}",end='\r')
 
-            
+            elif urls[ChoixVid][3] == "Compr2":
+                nomVid = urls[ChoixVid][1].replace(".","") + ".mp4"
+                Vid = os.path.join('./videos',nomVid)
+                VideoToFileBig(Vid,urls[ChoixVid][1])
+                os.remove(Vid)
+                print(f"Deleted: {file_path}",end='\r')
+            else:
+                print("Description invalide")
+
         if ChoixCloud == 3:
             for i in range(len(urls)):
                 print(i+1,urls[str(i+1)][1]+'\n')
