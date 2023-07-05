15834cad626ed4598a11bfb90439c461874e4992
SyRoxS <lmoreaumenanteau@gmail.com>
2023-07-05 14:51:44 +0200

    Working

--- main.py
+++ main.py
@@ -16,22 +16,42 @@ import time
 
 sys.path.append('tiktoka-studio-uploader-wanghaisheng-patch-4/examples')
 from ytuploadnoapi import upload
-
+from smb.SMBConnection import SMBConnection
 if __name__ == '__main__':
-    channel_url = "https://www.youtube.com/channel/UCyj7svz9hL15ciYwrV_wpLg"
-    path = "grostest/"
+    
+    local_videos_folder = 'videos/'
+    server_name = ''
+    server_ip = ''
+    server_share = ''
+    username = ''
+    password = ''
     while True:
-        time.sleep(10)
-        files = os.listdir(path)
-        if len(files) > 0:
+        
+        conn = SMBConnection(username, password, '', server_name)
+        conn.connect(server_ip)
+
+        # List files in the shared folder
+        files = conn.listPath(server_share, '/root/BBBBBB/videos/')
+        if len(files) > 1:
             for file in files:
-                actualfile = os.path.join(path, file)
-                FileToImages(file)
-                video_name = file + '.mp4'
-                ImagesToVideo(video_name)
-                upload(file,video_name,'Compr1',)
-                os.remove(actualfile)
+                print(file.filename)
+                if file.filename.endswith('.mp4'):
+                    local_file_path = os.path.join(local_videos_folder, file.filename)
+                    with open(local_file_path, 'wb') as f:
+                        conn.retrieveFile(server_share, '/root/BBBBBB/videos/'+file.filename, f)
+                    conn.deleteFiles(server_share, '/root/BBBBBB/videos/'+ file.filename)
+        conn.close()
+        
+        path = 'videos/'
+        files = os.listdir(path)
+        if len(files) > 1:
+            for file in files:
+                if file.endswith('.mp4'):
+                    video_name = os.path.join(path, file)
+                    upload(file,video_name,'Compr1',)
+                    os.remove(video_name)
         delscreenrec = os.listdir('screen-recording/')
         if len(delscreenrec) > 0:
             for screenrec in delscreenrec:
                 os.remove(os.path.join('screen-recording/', screenrec))
+        time.sleep(10)
