07ba1f7899ed90914fe5206599e15b4307940669
SyRoxS <lmoreaumenanteau@gmail.com>
2023-07-05 18:54:43 +0200

    Shows you what to change

--- linmain.py
+++ linmain.py
@@ -15,8 +15,7 @@ import time
 #This script is meant to be run on the machine without graphical interface
 
 if __name__ == '__main__':
-    channel_url = "https://www.youtube.com/channel/UCyj7svz9hL15ciYwrV_wpLg"
-    path = "/home/user/files/"
+    path = "FILE TO CONVERT"
     while True:
         time.sleep(10)
         files = os.listdir(path)
@@ -25,7 +24,7 @@ if __name__ == '__main__':
                 actualfile = os.path.join(path, file)
                 print(actualfile)
                 FileToImages(actualfile)
-                video_name = '/root/BBBBBB/videos/' + file + '.mp4'
+                video_name = 'WHERE TO SEND THE VIDEO' + file + '.mp4'
                 print(video_name)
                 ImagesToVideo(video_name)
                 os.remove(actualfile)
