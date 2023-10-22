9c2ed0e7feced35253eded9da68fbac520ec1ce8
SyRoxS1 <test.teste44momo@gmail.com>
2023-10-22 16:42:35 +0200

    added short option

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from VideoToFileNormal import VideoToFileNormal
 from VideoToFileBig import VideoToFileBig
 from GetYtVideosName import get_channel_videos
 from VideoDownloader import DLFromYt
+from RotateForShort import Rotate
 
 import os
 import time
@@ -18,24 +19,46 @@ sys.path.append('tiktoka-studio-uploader-wanghaisheng-patch-4/examples')
 from ytuploadnoapi import upload
 
 if __name__ == '__main__':
-    path = "PUT THE PATH WERE FILE THAT NEED TO BE PROCESSED INTO YOUTUBE VIDEOS ARE STORED"
+    short = True
+    path = "TEST"
     while True:
-        
-        files = os.listdir(path)
-        if len(files) > 0:
-            for file in files:
-                actualfile = os.path.join(path, file)
-                print(actualfile)
-                FileToImages(actualfile)
-                video_name = 'videos/' + file + '.mp4'
-                print(video_name)
-                ImagesToVideo(video_name)
-                
-                upload(file,video_name,'Compr1',)
-                os.remove(actualfile)
-                os.remove(video_name)
-        delscreenrec = os.listdir('screen-recording/')
-        if len(delscreenrec) > 0:
-            for screenrec in delscreenrec:
-                os.remove(os.path.join('screen-recording/', screenrec))
-        time.sleep(5) #some delay to avoid overloading the system
+        if short == True:
+            files = os.listdir(path)
+            if len(files) > 0:
+                for file in files:
+                    actualfile = os.path.join(path, file)
+                    print(actualfile)
+                    FileToImages(actualfile)
+                    Rotate()
+                    video_name = 'videos/' + file + '.mp4'
+                    print(video_name)
+                    ImagesToVideo(video_name)
+                    
+                    upload(file,video_name,'Compr1',)
+                    os.remove(actualfile)
+                    os.remove(video_name)
+            delscreenrec = os.listdir('screen-recording/')
+            if len(delscreenrec) > 0:
+                for screenrec in delscreenrec:
+                    os.remove(os.path.join('screen-recording/', screenrec))
+            time.sleep(5) #some delay to avoid overloading the system
+
+        else:
+            files = os.listdir(path)
+            if len(files) > 0:
+                for file in files:
+                    actualfile = os.path.join(path, file)
+                    print(actualfile)
+                    FileToImages(actualfile)
+                    video_name = 'videos/' + file + '.mp4'
+                    print(video_name)
+                    ImagesToVideo(video_name)
+                    
+                    upload(file,video_name,'Compr1',)
+                    os.remove(actualfile)
+                    os.remove(video_name)
+            delscreenrec = os.listdir('screen-recording/')
+            if len(delscreenrec) > 0:
+                for screenrec in delscreenrec:
+                    os.remove(os.path.join('screen-recording/', screenrec))
+            time.sleep(5) #some delay to avoid overloading the system
